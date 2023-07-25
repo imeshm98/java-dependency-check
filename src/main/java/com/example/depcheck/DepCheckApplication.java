fab0afcead70781ade85960d52d633d64baeef03
imeshm98 <imeshmofficial@gmail.com>
2023-07-25 11:33:35 +0530

    first commit

---
+++ src/main/java/com/example/depcheck/DepCheckApplication.java
@@ -0,0 +1,13 @@
+package com.example.depcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/depcheck/DepCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.depcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
